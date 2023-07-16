43f2397eb305004b7afbacb33b02c79af2c95b6f
PHUCDAT @DM <bluehairmylove2014@gmail.com>
2023-07-16 21:52:16 +0700

    ADD: layout homepage

--- client/src/components/layouts/Footer.js
+++ client/src/components/layouts/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
+        <div>
             
-        </footer>
+        </div>
     );
 }
 
---
+++ client/src/pages/Home/HomeData.js
@@ -0,0 +1,111 @@
+import stepIcon1 from '../../assets/images/graphics/step-1.png'
+import stepIcon2 from '../../assets/images/graphics/step-2.png'
+import stepIcon3 from '../../assets/images/graphics/step-3.png'
+
+import banner1 from '../../assets/images/intro/doodoosonic-0t7PNNZlTC0-unsplash.jpg';
+import banner2 from '../../assets/images/intro/clark-street-mercantile-qnKhZJPKFD8-unsplash.jpg';
+
+import customOption1 from '../../assets/images/intro/stephen-audu-eULG-K36phc-unsplash.jpg'
+import customOption2 from '../../assets/images/intro/hunters-race-MYbhN8KaaEc-unsplash.jpg'
+import customOption3 from '../../assets/images/intro/alvin-mahmudov-NSVJAAXOYHs-unsplash.jpg'
+
+export const steps = [
+    {
+        icon: stepIcon1,
+        title: 'SELECT YOUR FABRICS',
+        description: 'Pick the fabric that you want from our fabric collection.'
+    },
+    {
+        icon: stepIcon2,
+        title: 'CHOOSE YOUR STYLE',
+        description: 'Select the style that suits you best from our wide range of options.'
+    },
+    {
+        icon: stepIcon3,
+        title: 'MEASURE YOURSELF',
+        description: 'Provide your measurements and we will create a custom fit for you.'
+    }
+];
+export const customOptions = [
+    {
+        image: customOption1,
+        subTitle: 'MADE TO MEASURE AND DESIGNED BY YOU',
+        title: 'CUSTOM SHIRTS'
+    },
+    {
+        image: customOption2,
+        subTitle: 'DESIGN YOUR NEW FAVORITE SUITS',
+        title: 'CUSTOM SUITS'
+    },
+    {
+        image: customOption3,
+        subTitle: 'BE THE BEST DRESSED MAN AT THE WEDDING!',
+        title: 'WEDDING SUITS'
+    },
+];
+export const banners = [banner1, banner2];
+export const reviews = [
+    {
+        name: "John Doe",
+        avatar: "https://aldortio.sirv.com/Sunrise%20Assistant/Assistants/assistant%20(9).png",
+        title: "Great Experience",
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id aliquam tincidunt, nisl nunc tincidunt urna, id lacinia nunc nunc id nunc.",
+        rating: 5
+    },
+    {
+        name: "Jane Smith",
+        avatar: "https://aldortio.sirv.com/Sunrise%20Assistant/Assistants/assistant%20(8).png",
+        title: "Highly Recommend",
+        review: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed auctor, nunc id aliquam tincidunt, nisl nunc tincidunt urna, id lacinia nunc nunc id nunc.",
+        rating: 4
+    },
+    {
+        name: "John Doe",
+        avatar: "https://aldortio.sirv.com/Sunrise%20Assistant/Assistants/assistant%20(7).png",
+        title: "Great Experience",
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id aliquam tincidunt, nisl nunc tincidunt urna, id lacinia nunc nunc id nunc.",
+        rating: 5
+    },
+    {
+        name: "Jane Smith",
+        avatar: "https://aldortio.sirv.com/Sunrise%20Assistant/Assistants/assistant%20(6).png",
+        title: "Highly Recommend",
+        review: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed auctor, nunc id aliquam tincidunt, nisl nunc tincidunt urna, id lacinia nunc nunc id nunc.",
+        rating: 4
+    },
+    {
+        name: "John Doe",
+        avatar: "https://aldortio.sirv.com/Sunrise%20Assistant/Assistants/assistant%20(2).png",
+        title: "Great Experience",
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id aliquam tincidunt, nisl nunc tincidunt urna, id lacinia nunc nunc id nunc.",
+        rating: 5
+    },
+    {
+        name: "Jane Smith",
+        avatar: "https://aldortio.sirv.com/Sunrise%20Assistant/Assistants/assistant%20(3).png",
+        title: "Highly Recommend",
+        review: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed auctor, nunc id aliquam tincidunt, nisl nunc tincidunt urna, id lacinia nunc nunc id nunc.",
+        rating: 4
+    },
+    {
+        name: "John Doe",
+        avatar: "https://aldortio.sirv.com/Sunrise%20Assistant/Assistants/assistant%20(4).png",
+        title: "Great Experience",
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id aliquam tincidunt, nisl nunc tincidunt urna, id lacinia nunc nunc id nunc.",
+        rating: 5
+    },
+    {
+        name: "Jane Smith",
+        avatar: "https://aldortio.sirv.com/Sunrise%20Assistant/Assistants/assistant%20(5).png",
+        title: "Highly Recommend",
+        review: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed auctor, nunc id aliquam tincidunt, nisl nunc tincidunt urna, id lacinia nunc nunc id nunc.",
+        rating: 4
+    },
+    {
+        name: "John Doe",
+        avatar: "https://aldortio.sirv.com/Sunrise%20Assistant/Assistants/assistant%20(1).png",
+        title: "Great Experience",
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nunc id aliquam tincidunt, nisl nunc tincidunt urna, id lacinia nunc nunc id nunc.",
+        rating: 5
+    },
+];
