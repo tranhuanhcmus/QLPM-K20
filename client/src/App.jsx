7f43bc35f7a5c35ced3b601b4623a7c94696bc35
tranhuanhcmus <tranhuanbs02@gmail.com>
2023-07-19 10:07:28 +0700

    FIX/Restruct Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,65 +1,29 @@
 // Global style
 import "./App.scss";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/layouts/Header";
-import Footer from "./components/layouts/Footer";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Measure from "./pages/Measure/Measure";
 import { URLS } from "./constants/urls";
 import AuthPage from "./pages/AuthPage/AuthPage";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import MainLayout from "./pages/MainLayout";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route
-            exact
-            path={URLS.HOME_PAGE}
-            element={
-              <>
-                <Header />
-                <Home />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            path={URLS.ABOUT_PAGE}
-            element={
-              <>
-                <Header />
-                <About />
-                <Footer />
-              </>
-            }
-          />
-          <Route path={URLS.MEASURE} element={<Measure />} />
-          <Route
-            path={URLS.AUTH_PAGE}
-            element={
-              <>
-                <Header />
-                <AuthPage />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            path={URLS.CONTACT_PAGE}
-            element={
-              <>
-                <Header />
-                <Contact />
-                <Footer />
-              </>
-            }
-          />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path={URLS.MEASURE} element={<Measure />} />
+        <Route path={URLS.HOME_PAGE} element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path={URLS.ABOUT_PAGE} element={<About />} />
+          <Route path={URLS.AUTH_PAGE} element={<AuthPage />} />
+          <Route path={URLS.CONTACT_PAGE} element={<Contact />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
   );
 }
 
