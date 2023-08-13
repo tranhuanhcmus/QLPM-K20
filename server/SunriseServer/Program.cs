5e6319bec9ad55fefe8ba9fb66a35013749e4452
lyvandat <75932518+lyvandat@users.noreply.github.com>
2023-08-13 20:39:09 +0700

    use swagger in production

--- server/SunriseServer/Program.cs
+++ server/SunriseServer/Program.cs
@@ -96,11 +96,8 @@ builder.Services.AddUnitOfWork(options => options.UseSqlServer(builder.Configura
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("NgOrigins");
 
