62e388d8966b224c71036c147b214b1232470914
tranhuanhcmus <tranhuanbs02@gmail.com>
2023-08-20 17:52:45 +0700

    fix measure

--- client/src/pages/Measure/Measure.jsx
+++ client/src/pages/Measure/Measure.jsx
@@ -7,18 +7,19 @@ import {
   listTryProduct,
   listItemMeasureFilter,
 } from "../../assets/constant/index";
+import FabricStep from "./Step/FabricStep";
+import StyleStep from "./Step/StyleStep";
+
+
 
 const Measure = () => {
   const [selectedIndexProduct, setSelectedIndexProduct] = useState(0);
   const [isFrontView, setIsFrontView] = useState(true);
-  const [tagCloth, setTagCloth] = useState(0);
-  const navigate = useNavigate();
-  const steps=["Fabric","Style","Accent"]
-  const [currentStep,setCurrentStep]=useState(0)
 
-  const filteredProducts = listTryProduct.filter(
-    (item) => item.tags.indexOf(tagCloth) !== -1 || tagCloth === 0
-  );
+  const navigate = useNavigate();
+  const steps = ["Fabric", "Style"];
+  const [currentStep, setCurrentStep] = useState(0);
+
 
   return (
     <div className="measure-container">
@@ -28,56 +29,20 @@ const Measure = () => {
           Quay lại
         </button>
         <div className="steps">
-          {steps.map((item,index)=>{
-            return <div className={`step ${currentStep===index&&`current` }`}  >
-              <button onClick={()=>setCurrentStep(index)}>
-                {item}
-              </button>
-              <i className="fi fi-rr-angle-small-right"></i>
-            </div>
+          {steps.map((item, index) => {
+            return (
+              <div className={`step ${currentStep === index && `current`}`}>
+                <button onClick={() => setCurrentStep(index)}>{item}</button>
+                <i className="fi fi-rr-angle-small-right"></i>
+              </div>
+            );
           })}
         </div>
       </div>
 
       <div className="measure-body">
-        <div className="measure__filter--mn">
-          {listItemMeasureFilter &&
-            listItemMeasureFilter.map((item, index) => {
-              return (
-                <button
-                  className="measure__filter--it"
-                  key={index}
-                  onClick={() => setTagCloth(index)}
-                >
-                  {item.icon}
-                  <div>
-                    <p>{item.title}</p>
-                  </div>
-                </button>
-              );
-            })}
-        </div>
-        <div className="measure__choose--product">
-          {filteredProducts.map((item, index) => (
-            <div className="measure__choose--it" key={index}>
-              <img
-                src={item.cloth}
-                alt="product"
-                className={`${
-                  index === selectedIndexProduct
-                    ? "product--selected"
-                    : "product--unselected"
-                }`}
-                onClick={() => {
-                  setSelectedIndexProduct(index);
-                }}
-              />
-              <div>
-                <PriceText>{item.price} </PriceText>
-              </div>
-            </div>
-          ))}
-        </div>
+        {currentStep === 0 && <FabricStep />}
+        {currentStep === 1 && <StyleStep />}
         <div className="measure__swear--suit">
           {isFrontView ? (
             <img
