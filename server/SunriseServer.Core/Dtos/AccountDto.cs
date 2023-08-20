08d7941c7dec932ba4782e1c7d21ae0b8537579b
lyvandat <75932518+lyvandat@users.noreply.github.com>
2023-08-20 22:32:39 +0700

    remove all

--- server/SunriseServer.Core/Dtos/AccountDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SunriseServerCore.Dtos
-{
-    public class AccountDto
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
--- server/SunriseServer.Core/Models/RefreshToken.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SunriseServerCore.Models
-{
-    public class RefreshToken
-    {
-        public string Token { get; set; } = string.Empty;
-        public DateTime Created { get; set; } = DateTime.Now;
-        public DateTime Expires { get; set; }
-    }
-}
